e07d78455ca151715e2c48cc705a130f63449253
Sride Shankar <sridevishankar1161@gmail.com>
2024-02-27 18:09:00 +0530

    update README.md

---
+++ src/colorPalette.js
@@ -0,0 +1,24 @@
+var colorPalette = [
+  {
+    id: 0,
+    name: "Choose a color",
+    shades: [],
+  },
+  {
+    id: 1,
+    name: "Blue",
+    shades: ["cobalt", "sky", "midnight", "cyan"],
+  },
+  {
+    id: 2,
+    name: "Green",
+    shades: ["olive", "chartreuse", "dark", "bluegreen"],
+  },
+  {
+    id: 3,
+    name: "Red",
+    shades: ["crimson", "wine", "rose", "brick"],
+  },
+];
+
+export default colorPalette;
